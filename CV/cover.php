478271b0486a04ba81e1da70637c7d1a3237542c
KDemeul <kilian.demeule@gmail.com>
2014-12-18 19:42:46 +0100

    Update cover.php

--- CV/cover.php
+++ CV/cover.php
@@ -2,7 +2,7 @@
 	<div id="cover-title"> 
 		<span class="bold">KILIAN</span> &#8232; &#8232; <span class="light">DEMEULEMEESTER</span>
 		<div class="arrow">
-			<a href="#about" class="bold"></a>
+			<a href="#about" class="bold glyphicon glyphicon-chevron-down" ></a>
 		</div>
 	</div>
 </div>
