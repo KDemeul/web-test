be8d7d3080af36ef9729f5f983f1becffe1a58d3
KDemeul <kilian.demeule@gmail.com>
2015-01-22 11:28:38 +0100

    [Motiv] Creation

---
+++ CV_paper/motiv.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Motiv</title>
+	<!-- Bootstrap core CSS -->
+	<link href="css/bootstrap.css" rel="stylesheet">
+	<!-- Custom css -->
+	<link href="css/motiv.css" rel="stylesheet">
+	<link href="css/background.css" rel="stylesheet">
+	<link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
+	<link href="css/bootstrap-social.css" rel="stylesheet">
+</head>
+<body>
+	<div class="letter">
+		<div class="content">
+			<div class="me">
+				<span class="bold text-md">Kilian Demeulemeester</span> <br>
+				3 rue Gustave Courbet <br>
+				34090 Montpellier <br>
+				<span class="fa fa-phone"></span> (+33) 06.88.55.29.37 <br>
+				<span class="fa fa-at"></span> kilian.demeule@gmail.com
+			</div>
+			<div class="separator-md"></div>
+			<div class="them">
+				<span class="bold text-md">Ubisoft</span> <br>
+				Dans le cadre de "Ubisoft Graduate Program"
+			</div>
+			<div class="separator-md"></div>
+			<div class="object">
+				Candidature pour le "Graduate Program – Graduate Programmeur Gameplay"
+			</div>
+			<div class="separator-md"></div>
+			<div class="main">
+				<p> Madame, Monsieur </p>
+				<p> 
+					Votre entreprise à ouvert son "Graduate Program", prouvant son dynamisme et offrant une <em>incroyable</em> opportunité aux jeunes diplômés. 
+				</p>
+			</div>
+			<div class="separator-md"></div>
+			<div class="signature">
+			</div>
+		</div>
+	</div>
+</body>
+</html>
